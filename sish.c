48cb2a2cedd66c348f4b79d2503db6810467de8b
Jackson Saenz <saenzjack1@icloud.com>
2023-03-26 22:29:39 -0500

    work pls

--- sish.c
+++ sish.c
@@ -5,37 +5,29 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-
-
 size_t MAX = 1024; // max length
 int flag = 1;
 
-void run(char *args[])
-{
+void run(char *args[]) {
     pid_t pid;
-    if (strcmp(args[0], "exit") ==0){
+    if (strcmp(args[0], "exit") == 0) {
         flag = 0;
-    }else if(strcmp(args[0], "cd") == 0)
-    {
-        if (chdir(args[1]) != 0)
-        {
+    } else if (strcmp(args[0], "cd") == 0) {
+        if (chdir(args[1]) != 0) {
             perror("Error changing directory");
         }
-    }else{
+    } else {
         pid = fork();
-        if(pid < 0){
+        if (pid < 0) {
             perror("Fork Failed");
-        }
-        else if (pid == 0){
-            if (execvp(args[0], args) < 0){
+        } else if (pid == 0) {
+            if (execvp(args[0], args) < 0) {
                 perror("Error executing command");
             }
-        }else{
+        } else {
             waitpid(pid, NULL, 0);
         }
     }
-
-
 }
 
 void piper(char *args[]) {
@@ -43,101 +35,76 @@ void piper(char *args[]) {
     pipe(fd);
 
     pid_t pid = fork();
-
-    if (pid == -1) {
-        perror("Fork failed");
-        exit(1);
+    if (pid < 0) {
+        perror("Fork Failed");
     } else if (pid == 0) {
-        // Child process
-        close(fd[0]);
-        dup2(fd[1], STDOUT_FILENO);
+        dup2(fd[1], 1);
         close(fd[1]);
-
-        // Loop through args and print each one individually
-        for (int i = 0; args[i] != NULL; i++) {
-            printf("%s ", args[i]);
-        }
-        printf("\n");
-
-        // Redirect stdin to the read end of the pipe
-        dup2(fd[0], STDIN_FILENO);
-
-        // Execute the command
-        if (execvp(args[0], args) == -1) {
-            perror("Error executing command");
-            exit(1);
-        }
+        close(fd[0]);
+        run(args);
+        exit(0);
     } else {
-        // Parent process
-        close(fd[1]);
-
-        // Wait for the child process to finish
-        waitpid(pid, NULL, 0);
-
-        // Close the read end of the pipe
+        dup2(fd[0], 0);
         close(fd[0]);
+        close(fd[1]);
+        waitpid(pid, NULL, 0);
     }
 }
 
-char * tokenize(char *input)
-{
+char *tokenize(char *input) {
     int i;
     int j = 0;
-    char *cleaned = (char* )malloc((MAX *2) * sizeof(char));
+    char *cleaned = (char *) malloc((MAX * 2) * sizeof(char));
 
-    //clean input to allow for easy reading
-    for (i = 0; i< strlen(input); i++){
-        if (input[i] == '|'){
+    // clean input to allow for easy reading
+    for (i = 0; i < strlen(input); i++) {
+        if (input[i] == '|') {
             cleaned[j++] = ' ';
             cleaned[j++] = input[i];
             cleaned[j++] = ' ';
-        }
-        else{
+        } else {
             cleaned[j++] = input[i];
         }
     }
     cleaned[j++] = '\0';
 
     char *end;
-    end = cleaned + strlen(cleaned)-1;
+    end = cleaned + strlen(cleaned) - 1;
     end--;
-    *(end+1) = '\0';
+    *(end + 1) = '\0';
 
     return cleaned;
 }
 
-int main(void)
-{
+int main(void) {
     char *args[MAX];
 
-    while (flag){
-            printf("sish> ");
+    while (flag) {
+        printf("sish> ");
 
-            char *input = malloc(MAX * sizeof(char));
-            getline(&input, &MAX, stdin);
+        char *input = malloc(MAX * sizeof(char));
+        getline(&input, &MAX, stdin);
 
-            char *tokens;
-            tokens = tokenize(input);
+        char *tokens;
+        tokens = tokenize(input);
 
-            char *arg = strtok(tokens, " ");
-            int i = 0;
-            while(arg) { 
-                if (*arg == '|'){
-                    args[i] = NULL;
-                    piper(args);
-                    i = 0;
-                }else{
-                    args[i] = arg;
-                    i++;
-                }
-                arg = strtok(NULL, " ");
+        char *arg = strtok(tokens, " ");
+        int i = 0;
+        while (arg) {
+            if (*arg == '|') {
+                args[i] = NULL;
+                piper(args);
+                i = 0;
+            } else {
+                args[i] = arg;
+                i++;
             }
-            args[i] = NULL;
+            arg = strtok(NULL, " ");
+        }
+        args[i] = NULL;
 
-            if(i>0){
-                run(args);
-            }
-            free(input);
+        run(args);
+        free(input);
     }
     return 0;
 }
