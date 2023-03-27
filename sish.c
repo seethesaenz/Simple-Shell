d5b46219df41283e240cf51e0b7fcd444f0c046a
Jackson Saenz <saenzjack1@icloud.com>
2023-03-27 14:08:20 -0500

    reverrttt

--- sish.c
+++ sish.c
@@ -37,35 +37,24 @@ void run(char *args[]) {
     }
 }
 
-void piper(char *args1[], char *args2[]) {
+void piper(char *args[], int size) {
     int fd[2];
     pipe(fd);
 
-    pid_t pid1 = fork();
-    if (pid1 < 0) {
+    pid_t pid = fork();
+    if (pid < 0) {
         perror("Fork Failed");
-    } else if (pid1 == 0) {
+    } else if (pid == 0) {
         dup2(fd[1], 1);
         close(fd[1]);
         close(fd[0]);
-        execvp(args1[0], args1);
+        execvp(args[0], args);
         exit(0);
     } else {
-        pid_t pid2 = fork();
-        if (pid2 < 0) {
-            perror("Fork Failed");
-        } else if (pid2 == 0) {
-            dup2(fd[0], 0);
-            close(fd[0]);
-            close(fd[1]);
-            execvp(args2[0], args2);
-            exit(0);
-        } else {
-            close(fd[0]);
-            close(fd[1]);
-            waitpid(pid1, NULL, 0);
-            waitpid(pid2, NULL, 0);
-        }
+        dup2(fd[0], 0);
+        close(fd[0]);
+        close(fd[1]);
+        waitpid(pid, NULL, 0);
     }
 }
 
@@ -95,48 +84,38 @@ char *tokenize(char *input) {
 }
 
 int main(void) {
-    char *args1[MAX];
-    char *args2[MAX];
+    char *args[MAX];
 
     while (flag) {
-        memset(args1, 0, sizeof(args1));
-        memset(args2, 0, sizeof(args2));
+        memset(args, 0, sizeof(args));
 
         printf("sish> ");
 
         char *input = malloc(MAX * sizeof(char));
         getline(&input, &MAX, stdin);
 
+
         char *tokens;
         tokens = tokenize(input);
 
         char *arg = strtok(tokens, " \n");
         int i = 0;
-        int j = 0;
-        int pipe_found = 0;
         while (arg) {
             if (*arg == '|') {
-                pipe_found = 1;
+                args[i] = NULL;
+                piper(args, i);
                 i = 0;
-            } else if (pipe_found) {
-                args2[i] = arg;
-                i++;
             } else {
-                args1[i] = arg;
+                args[i] = arg;
                 i++;
             }
             arg = strtok(NULL, " \n");
         }
-        args1[i] = NULL;
-        args2[j] = NULL;
-
-        if (pipe_found) {
-            piper(args1, args2);
-        } else {
-            run(args1);
-        }
+        args[i] = NULL;
+        run(args);    
 
         free(input);
+
     }
     return 0;
 }
