9042ded6d88eb3e69f5a8e942898a9bbc8e49d08
Jackson Saenz <saenzjack1@icloud.com>
2023-03-27 13:04:40 -0500

    work pls

--- sish.c
+++ sish.c
@@ -98,6 +98,7 @@ int main(void) {
 
         char *input = malloc(MAX * sizeof(char));
         getline(&input, &MAX, stdin);
+        input[strcspn(input, "\n")] = "\0";
 
         char *tokens;
         tokens = tokenize(input);
