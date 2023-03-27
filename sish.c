00e8ef7128170375558442ce0373885c6693e77c
Jackson Saenz <saenzjack1@icloud.com>
2023-03-27 12:24:06 -0500

    work pls

--- sish.c
+++ sish.c
@@ -104,27 +104,28 @@ int main(void) {
         char *input = malloc(MAX * sizeof(char));
         getline(&input, &MAX, stdin);
 
-        char *tokens;
-        tokens = tokenize(input);
-
-        char *arg = strtok(tokens, " \n");
-        int i = 0;
-        while (arg) {
-            if (*arg == '|') {
-                args[i] = NULL;
-                piper(args, i);
-                i = 0;
-            } else {
-                args[i] = arg;
-                i++;
+        // Split input by pipe character
+        char *cmd = strtok(input, "|");
+        while (cmd) {
+            // Tokenize each command individually
+            char *tokens = strtok(cmd, " \n");
+            int i = 0;
+            while (tokens) {
+                args[i++] = tokens;
+                tokens = strtok(NULL, " \n");
             }
-            arg = strtok(NULL, " \n");
+            args[i] = NULL;
+
+            // Run the command
+            piper(args, i);
+
+            // Move on to the next command
+            cmd = strtok(NULL, "|");
         }
-        args[i] = NULL;
-        run(args);    
 
         free(input);
-        fflush(stdin);
     }
+
     return 0;
 }
+
