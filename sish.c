79a5298036d1adf9c0f0af9aa0b6818f10131943
Jackson Saenz <saenzjack1@icloud.com>
2023-03-27 15:09:51 -0500

    added all of history in one go, should work first try

--- sish.c
+++ sish.c
@@ -6,8 +6,65 @@
 #include <errno.h>
 #include <sys/wait.h>
 
+#define MAX_HISTORY 100
 size_t MAX =  1024; // max length
 int flag = 1;
+char *history[MAX_HISTORY];
+int history_start = 0;
+int history_end = 0;
+int history_count = 0;
+
+//add entry to history array
+void add(char *cmd){
+    //removing oldest entry if full
+    if (history_count == MAX_HISTORY){
+        free(history[history_start]);
+        history_start = (history_start + 1) % MAX_HISTORY;
+        history_count--;
+    }
+    //adding entry
+    history[history_end] = strdup(cmd);
+    history_end = (history_end + 1) % MAX_HISTORY;
+    history_count++;
+}
+//prints history
+void print_history(){
+    int i;
+    int j = history_start;
+    for (i = 0; i < history_count; i++){
+        printf("%d %s\n", i, history[j]);
+        j = (j + 1) % MAX_HISTORY;
+    }
+}
+//clears history
+void clear_history(){
+    int i;
+    int j = history_start;
+    for (i = 0; i < history_count; i++){
+        free(history[j])
+        j = (j + 1) % MAX_HISTORY;
+    }
+    history_start = 0;
+    history_count = 0;
+    history_end = 0;
+}
+//history handler
+void history(char *args[]){
+    if (args[1] == NULL){
+        print_history();
+    }else if (strcmp(args[1], "-c") == 0){
+        clear_history();
+    }else {
+        int offset = atoi(args[1]);
+        if(offset < 0 || offset >= history_count) {
+            printf("invalid offset\n");
+            return;
+        }
+        int index = (history_start+offset) % MAX_HISTORY;
+        run_history(history[index]);
+    }
+}
+
 
 void piper(char *args[], int size) {
     int i;
@@ -27,8 +84,11 @@ void piper(char *args[], int size) {
         if(chdir(args[1]) != 0){
             perror("Error changing directory");
         }
+    }else if(strcmp(args[0], "history") == 0){
+        history(args);
+        break;
     }
-
+    //#number of commands
     for (i = 0; i < size; i++) {
         if (args[i] == NULL) {
             count++;
@@ -36,44 +96,44 @@ void piper(char *args[], int size) {
     }
 
     for (i = 0; i < count; i++) {
-
+        // start and end , j is end of command
         cmd_start = j;
         while (args[j] != NULL) {
             j++;
         }
 
-
+        // pipe for every command except last
         if (i < count - 1) {
             pipe(fd);
         }
 
-
+        //forking 
         pid_t pid = fork();
         if (pid < 0) {
             perror("Fork Failed");
         } else if (pid == 0) {
-
+            //redirecting input of all except first
             if (i > 0) {
                 dup2(in_fd, 0);
                 close(in_fd);
             }
 
-
+            //output all except last
             if (i < count - 1) {
                 dup2(fd[1], 1);
                 close(fd[1]);
                 close(fd[0]);
             }
 
-
+            //run cmd
             execvp(args[cmd_start], &args[cmd_start]);
             exit(0);
         } else {
-
+            //close input from prior 
             if (i > 0) {
                 close(in_fd);
             }
-
+            //setup input for next
             if (i < count - 1) {
                 in_fd = fd[0];
                 close(fd[1]);
@@ -108,6 +168,27 @@ char *tokenize(char *input) {
     *(end + 1) = '\0';
 
     return cleaned;
+}
+//reruns history command at index
+run_history(char *input){
+    char *args[MAX];
+    char *tokens;
+    tokens = tokenize(input);
+
+    char *arg = strtok(tokens, " \n");
+    int i = 0;
+        while (arg) {
+            if (*arg == '|') {
+                args[i] = NULL;
+                i++;
+            } else {
+                args[i] = arg;
+                i++;
+            }
+            arg = strtok(NULL, " \n");
+        }
+    args[i] = NULL;
+    piper(args, i);
 }
 
 int main(void) {
